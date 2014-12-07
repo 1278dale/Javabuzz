156b2e2c32edaec73ffe7035a15c749dc3adcfe7
1278dale <dalestevens1278@gmail.com>
2014-12-07 22:23:25 +0000

    Sorted git error to wrong directory

---
+++ spec/javabuzzSpec.js
@@ -0,0 +1,27 @@
+describe("Javabuzz", function(){
+
+  var javabuzz;
+
+beforeEach(function){
+  javabuzz = new javabuzz();
+};
+
+  describe('knows when a number is', function(){
+
+    it('divisible by 3', function(){
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    describe('knows when a number is NOT', function(){
+      it('divisible by 3', function){
+        expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+      });
+
+    });
+
+  });
+
+});
+
+
+
